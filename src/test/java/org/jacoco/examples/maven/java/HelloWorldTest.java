2ccda4d343876cc6ed8ffa6b48093222bca03cd5
kmprasanth85 <32164400+kmprasanth85@users.noreply.github.com>
2021-05-12 10:13:28 +0530

    Update HelloWorldTest.java

--- src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
@@ -16,7 +16,7 @@ public class HelloWorldTest {
 
 	@Test
 	public void testGetMessage() {
-		assertEquals("Hello World!", subject.getMessage(false));
+		assertEquals("Hello World!0000", subject.getMessage(false));
 	}
 
 }
